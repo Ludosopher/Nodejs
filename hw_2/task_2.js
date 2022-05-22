f2e1cd6da68c7b38dba7a5b45905951d66445284
Viktor Alikin <ludosopher@mail.ru>
2022-05-22 22:09:12 +0300

    added clearInterval

--- hw_2/task_2.js
+++ hw_2/task_2.js
@@ -2,9 +2,8 @@ const EventEmitter = require("events");
 const eventEmitter = new EventEmitter();
 
 const countDown = (time_str) => {
-    var time_array = time_str.split('-');
-    time_arr = time_array.reverse();
-    var date = new Date(time_arr[0], time_arr[1]-1, time_arr[2], time_arr[3]);
+    var time_arr = time_str.split('-');
+    var date = new Date(time_arr[4], time_arr[3]-1, time_arr[2], time_arr[1], time_arr[0]);
     var now = new Date();
     var diff = date - now;
 
@@ -16,12 +15,11 @@ const countDown = (time_str) => {
 }
 
 
-const times = ["20-21-05-2022", "20-21-05-2022"];
-
+const times = ["05-22-22-05-2022", "06-22-22-05-2022", "07-22-22-05-2022"];
 
 eventEmitter.on("timer", (payload) => { console.log(payload) });
 
 for (let i = 0; i + 1 <= times.length; i++) {
     const timerId = setInterval(() => eventEmitter.emit("timer", ''.concat(i+1, ': ', countDown(times[i]))), 1000);
+    setTimeout(() => clearInterval(timerId), (countDown(times[i]) + 2) * 1000);
 }
-
